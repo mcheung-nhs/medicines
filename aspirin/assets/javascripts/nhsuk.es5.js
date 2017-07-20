cea41faadacf574e6317ba8c420167a95329ed5b
Michael Cheung <michaelcheung@nhs.net>
2017-07-20 07:12:24 +0100

    :pill: Added aspirin flat pages

---
+++ aspirin/assets/javascripts/nhsuk.es5.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+var $ = require('jquery');
+var cookieMessage = require('./modules/cookie-message');
+var feedbackForm = require('./modules/feedback-form');
+var analytics = require('./modules/analytics');
+var labelFocus = require('./modules/label-focus');
+var labelSelect = require('./modules/label-select');
+var tabs = require('./modules/tabs');
+
+cookieMessage('global-cookies-banner');
+
+$(function () {
+  feedbackForm.init();
+  analytics.init();
+  labelFocus.init();
+  labelSelect.init();
+  tabs.init();
+});
+
